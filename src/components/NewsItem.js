3f2d8627eaad6c3e6c44f402b12283ce832fd332
Arpit Hamjade <arpitshirishhamjade@gmail.com>
2024-02-09 13:19:19 +0530

    added date component

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,22 +1,47 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class NewsItem extends Component {
-
   render() {
-    let {title,description,imageUrl,newsUrl}=this.props;
+    let { title, description, imageUrl, newsUrl, author, date,source } = this.props;
     return (
-      <div className="my-3" >
-        <div className="card" style={{width: "18rem"}}>
-                <img src={imageUrl} className="card-img-top" alt="..."/>
-                <div className="card-body">
-                    <h5 className="card-title">{title}</h5>
-                    <p className="card-text">{description}</p>
-                    <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
-                </div>
+      <div className="my-3">
+        <div className="card" style={{ width: "18rem" }}>
+        <span className="position-absolute top-0  translate-middle badge rounded-pill bg-info" style={{left:'80%',zIndex: '1'}} >
+                {source}
+              </span>
+          <img
+            src={
+              !imageUrl
+                ? "	https://images.moneycontrol.com/static-mcnews/2024/02/sensex_nifty_market-770x433.jpg"
+                : imageUrl
+            }
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">
+              {title}
+            </h5>
+            <p className="card-text">{description}</p>
+            <p className="card-text">
+              <small className="text-body-secondary">
+                By {!author ? "unknown" : author} on{" "}
+                {new Date(date).toGMTString()}{" "}
+              </small>
+            </p>
+            <a
+              rel="noreferrer"
+              href={newsUrl}
+              target="_blank"
+              className="btn btn-sm btn-dark"
+            >
+              Read More
+            </a>
+          </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default NewsItem
+export default NewsItem;
